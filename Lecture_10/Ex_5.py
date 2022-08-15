b7e426961b3fff663239377c10d0faea5cdff2d5
atakancoban <ac223au@student.lnu.se>
2022-08-15 15:09:18 +0300

    :construction: Work on grid search for NN parameters

--- Lecture_10/Ex_5.py
+++ Lecture_10/Ex_5.py
@@ -1,12 +1,31 @@
 # ---- Exercise 5 - Fashion MNIST Neural Network ---- #
 
 # --- Imports --- #
-from tensorflow import keras
+from sklearn.model_selection import GridSearchCV
 from keras.datasets import fashion_mnist as mnist
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import to_categorical
+from scikeras.wrappers import KerasClassifier
 import numpy as np
+
+# --- Functions --- #
+def create_nn(
+    activation='sigmoid', optimizer='SGD', hidden_nodes=64
+):
+    # -- Build dimensions.
+    clf = Sequential([
+        Dense(hidden_nodes, activation=activation, input_shape=(784,)),
+        Dense(hidden_nodes, activation=activation),
+        Dense(10, activation=activation)
+    ])
+
+    # -- Compile Neural Network.
+    clf.compile(
+        optimizer=optimizer,
+        loss='mse',
+        metrics=['accuracy']
+    )
 
 # --- Part 1 - Load & prepare dataset --- #
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -16,34 +35,14 @@ y_train, y_test = to_categorical(y_train), to_categorical(y_test)
 X_train = X_train.reshape((-1, 784))
 X_test = X_test.reshape((-1, 784))
 
-# --- Part 2 - Prepare Neural Network --- #
-# -- Build dimensions.
-clf = Sequential([
-    Dense(64, activation='sigmoid', input_shape=(784,)),
-    Dense(64, activation='sigmoid'),
-    Dense(10, activation='sigmoid')
-])
+# --- Part 2 - Grid search for best hyper-parameters --- #
+params = {
+    'activation': ['sigmoid', 'relu', 'softmax'],
+    'optimizer': ['SGD', 'Adam'],
+    'hidden_nodes': [16, 32, 64]
+}
 
-# -- Compile Neural Network.
-clf.compile(
-    optimizer='SGD',
-    loss='mse',
-    metrics=['accuracy']
-)
-
-# -- Train Neural Network.
-load_weights = False
-
-if load_weights:
-    clf.load_weights('clf/fashion_mnist')
-else:
-    # -- Train.
-    clf.fit(
-        X_train,
-        y_train,
-        epochs=10,
-        batch_size=64,
-    )
-
-    # -- Save to disk.
-    clf.save_weights('clf/fashion_mnist')
+clf = KerasClassifier(model=create_nn, epochs=10, batch_size=64)  # Wrap Keras model for sci-kitlearn
+grid = GridSearchCV(clf, params, n_jobs=-1)
+grid.fit(X_train, y_train)
+clf = grid.best_estimator_
