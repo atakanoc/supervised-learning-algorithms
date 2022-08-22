388a6e51b1869396b9547f6ea471538655cb0eb7
atakancoban <ac223au@student.lnu.se>
2022-08-22 16:19:07 +0300

    :ambulance: Grid search now uses validation set

--- Lecture_10/Ex_5.py
+++ Lecture_10/Ex_5.py
@@ -10,6 +10,9 @@ from scikeras.wrappers import KerasClassifier
 import numpy as np
 
 # --- Functions --- #
+import a3
+
+
 def create_nn(
     activation='sigmoid', optimizer='SGD', hidden_nodes=64
 ):
@@ -32,32 +35,45 @@ def create_nn(
 # --- Part 1 - Load & prepare dataset --- #
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 y_train, y_test = to_categorical(y_train), to_categorical(y_test)
+X_valid, y_valid = X_train[50000:], y_train[50000:]
+X_train, y_train = X_train[:50000], y_train[:50000]
 
 # -- Flatten X.
 X_train = X_train.reshape((-1, 784))
 X_test = X_test.reshape((-1, 784))
+X_valid = X_valid.reshape((-1, 784))
 
 # --- Part 2 - Grid search for best hyper-parameters --- #
-params = {
-    'activation': ['sigmoid', 'relu'],
-    'optimizer': ['SGD', 'Adam'],
-    'hidden_nodes': [128, 256, 512]
-}
+# TODO: Make grid-search optional with loading instead. Model should not be trained.
+# TODO: Training should also be optional allowing you to load weights instead.
 
-# TODO: Make grid-search optional with loading instead.
-for a in params['activation']:
-    for o in params['optimizer']:
-        for n in params['hidden_nodes']:
-            clf = create_nn(a, o, n)
-            clf.fit(X_train, y_train, batch_size=64, epochs=10)
-            print(f"Model = {a}, {o}, {n}")
-            clf.evaluate(X_test, y_test)
-            print("\n\n")
+search = True
+if search:  # TODO: Save pickle
 
+    # -- Grid of hyper-parameters.
+    params = {
+        'activation': ['sigmoid', 'relu'],
+        'optimizer': ['SGD', 'Adam'],
+        'hidden_nodes': [128, 256, 512]
+    }
 
-# clf = KerasClassifier(model=create_nn, epochs=10, batch_size=64,
-#                       activation=None, hidden_nodes=None)  # Wrap Keras model for sci-kitlearn
-# grid = GridSearchCV(clf, params, n_jobs=-1, verbose=3)
-# grid.fit(X_train, y_train)
-# clf = grid.best_estimator_
-# print(clf.get_params())
+    # -- Grid search.
+    best_params = []
+    best_accuracy = 0
+    for a in params['activation']:
+        for o in params['optimizer']:
+            for n in params['hidden_nodes']:
+                print(f"Model = {a}, {o}, {n}")
+                temp_clf = create_nn(a, o, n)
+                temp_clf.fit(X_train, y_train, batch_size=64, epochs=1)  # Low epoch for quick testing.
+                accuracy = temp_clf.evaluate(X_valid, y_valid)[1]
+
+                if accuracy > best_accuracy:
+                    best_accuracy = accuracy
+                    best_params = [a, o, n]
+
+                print("\n\n")
+
+    a3.save_pickle("pickles/best_params.pickle", best_params)
+else:  # TODO: Load pickle
+    pass
